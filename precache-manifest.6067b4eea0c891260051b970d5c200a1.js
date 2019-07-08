d6f94ef21f38718f4f80570ac50d7cfa1077a327
Aleksandra Filipiak <aleksandra.filipiak12@gmail.com>
2019-07-08 12:19:26 +0200

    Updates

---
+++ precache-manifest.6067b4eea0c891260051b970d5c200a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee8e9c97d7c70b919bcb78e80eb16b7",
+    "url": "/zaginioneznalezione/index.html"
+  },
+  {
+    "revision": "11c13876975e31208346",
+    "url": "/zaginioneznalezione/static/css/main.79ced010.chunk.css"
+  },
+  {
+    "revision": "0b4f83835c5e6d217177",
+    "url": "/zaginioneznalezione/static/js/2.050fade7.chunk.js"
+  },
+  {
+    "revision": "11c13876975e31208346",
+    "url": "/zaginioneznalezione/static/js/main.c13fa405.chunk.js"
+  },
+  {
+    "revision": "71e8356e78673cde1ed0",
+    "url": "/zaginioneznalezione/static/js/runtime~main.236fe0f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zaginioneznalezione/static/media/logo.5d5d9eef.svg"
+  }
+]);
