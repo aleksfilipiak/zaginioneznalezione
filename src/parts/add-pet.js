43a3da14ba07da2eafabc11e9027104e66230dba
Aleksandra Filipiak <aleksandra.filipiak12@gmail.com>
2019-07-11 22:59:48 +0200

    add form2

--- src/parts/add-pet.js
+++ src/parts/add-pet.js
@@ -13,30 +13,41 @@ class AddPet extends Component {
             <div className='add-pet'>                
                <h1>Dodaj zwierzę</h1> 
                <form>
-                    <label>Dodaj zdjęcie</label>
-                    <input name='add-photo' type='file' ></input>
-                    {this.state.lost ? <label>Czas zagubienia:</label> : <label>Czas znalezienia:</label>}
-                    <input name='date' type='datetime-local'></input>
-                    <label>Miejsce</label>
-                    <input name='place'></input>
-                    <label>Podstawowe informacje o zwierzęciu:</label>
-                    <label>Imię:</label>
-                    <input type='checkbox' name='name' value='Nie znam'></input>
-                    <input type='text' name='name' placeholder='Wpisz imię'></input>
-                    <label>Wzrost:</label>
-                    <input type='radio' name='size'/><p>Do 30cm</p>
-                    <input type='radio' name='size' /><p>Między 30cm a 70cm</p>
-                    <input type='radio' name='size' /><p>Powyżej 70cm</p>
-                    <label>Płeć:</label>
-                    <input type='radio' name='gender' value='unknown'/><p>Nie znam</p>
-                    <input type='radio' name='gender' value='male'/><p>Samiec</p>
-                    <input type='radio' name='gender' value='female'/><p>Samica</p>
-                    <label>Dodatkowe informacje</label>
-                    <textarea name='more-info' placeholder='Jak wyglądało zdarzenie, co zwierzę miało przy sobie, jak się zachowywało...'></textarea>
-                    <label>Forma kontaktu</label>
-                    <input type='checkbox' name='contact' value='phone'/>Telefon
-                    <input type='checkbox' name='contact' value='e-mail'/>E-mail
-                    <input name='submit' type='submit' value='Dodaj'></input>
+                   <div className='add-photo-holder '>
+                        <label>Dodaj zdjęcie</label>
+                        <div id='add-photo'></div>
+                        <input name='add-photo' type='file' ></input><br/>
+                    </div>
+                    <div className='info-holder'>
+                        <div className='add-place-and-time'>
+                            {this.state.lost ? <label>Czas zagubienia:</label> : <label>Czas znalezienia:</label>}
+                            <input name='date' type='datetime-local'></input><br/>
+                            <label>Miejsce</label>
+                            <input name='place' type='text'></input><br/>
+                        </div>
+                        <div className='basic-info'>
+                            <label>Podstawowe informacje o zwierzęciu:</label><br/>
+                            <label>Imię:</label>
+                            <input type='checkbox' name='name' value='unknown'/>Nie znam <span>lub</span>
+                            <input type='text' name='name' placeholder='Wpisz imię'></input><br/>
+                            <label>Wzrost:</label>
+                            <input type='radio' name='size'/>Do 30cm
+                            <input type='radio' name='size' />Między 30cm a 70cm
+                            <input type='radio' name='size' />Powyżej 70cm<br/>
+                            <label>Płeć:</label>
+                            <input type='radio' name='gender' value='unknown'/>Nie znam
+                            <input type='radio' name='gender' value='male'/>Samiec
+                            <input type='radio' name='gender' value='female'/>Samica<br/>
+                            <label>Dodatkowe informacje</label><br/>
+                            <textarea name='more-info' placeholder='Jak wyglądało zdarzenie, co zwierzę miało przy sobie, jak się zachowywało...'></textarea><br/>
+                        </div>
+                    </div>
+                    <div className='contact-holder col-12'>
+                      <label>Forma kontaktu</label>
+                        <input type='checkbox' name='contact' value='phone'/>Telefon
+                        <input type='checkbox' name='contact' value='e-mail'/>E-mail<br/>
+                       <input name='submit' type='submit' value='Dodaj'></input>
+                    </div>
                </form>
             </div>
         );
